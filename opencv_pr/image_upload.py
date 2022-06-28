5ff878c3b817be2b2bce113d451dea56f0591284
migo <migo@migos-MacBook-Pro.local>
2022-06-28 12:00:43 +0900

    open cv + fastapi resizing

--- opencv_pr/image_upload.py
+++ opencv_pr/image_upload.py
@@ -2,31 +2,42 @@ import numpy as np
 import cv2
 from fastapi import FastAPI, File, UploadFile
 import uvicorn
-from operator import mul
+import asyncio
+
 app = FastAPI()
 
-def prepare(image):
-    IMG_SIZE = 224
-    new_array = cv2.resize(image, (IMG_SIZE, IMG_SIZE)) 
-    return new_array.reshape(-1, IMG_SIZE,IMG_SIZE,3)
+async def prepare(image_files:list[UploadFile]):
+    tasks = [asyncio.create_task(image.read()) for image in image_files]
+    contents = await asyncio.gather(*tasks)
+    img_list = []
+    for content in contents:
+        nparr = np.frombuffer(content,np.uint8)
+        img = cv2.imdecode(nparr,cv2.IMREAD_COLOR)
+        height,width,channel = img.shape[:3]
+        
+        #resize
+        while height * width * channel > 1024 * 1024 * 2:
+            height *= 0.9
+            width *= 0.9
+        resized_img = cv2.resize(
+                            img, 
+                            (int(width),int(height)),
+                            interpolation=cv2.INTER_AREA
+                                )
+        print(height*width*channel)
+        img_list.append(resized_img)
+    return img_list              
 
 
 @app.post("/")
-async def root(file: UploadFile = File(...)):
-    content = await file.read()
-    nparr = np.frombuffer(content, np.uint8)
-    img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
-    height,width,channel = img.shape[:3]
-    print(height*width*channel)
-    #resize
-    dst1 = cv2.resize(img, (int(width*0.5),int(height*0.5)),\
-                     interpolation=cv2.INTER_AREA)
-                     
-    print(dst1.shape[0]*dst1.shape[1]*dst1.shape[2])
-
-    cv2.imshow("small",dst1)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+async def root(files: list[UploadFile] = File(...)):
+    resized_imgs = await prepare(files)
+ 
+    for i in resized_imgs:
+        cv2.imshow("small",i)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+        
     
     return 2
 
